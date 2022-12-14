7d5c87b14fce9aac07bfac9bc79ed90cdf0517b8
junghun bak <wjdgnsi79@naver.com>
2022-12-14 13:52:33 +0900

    1214

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var timeCountLable: UILabel!
+    @IBOutlet weak var timeBar: UIProgressView!
+    @IBOutlet weak var timeCountTest: UILabel!
+    
+    let eggTimes = ["Soft":300 , "Medium":350 , "Hard": 720]
+    
+    var timeCount = 0
+    var percnetCount = 0
+    var zeroCount = 0
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender : UIButton) {
+        let hardness = sender.currentTitle
+        let hardnessTime = eggTimes[hardness!]
+        
+        timeCount += Int(hardnessTime!)
+        percnetCount += Int(hardnessTime!)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTime), userInfo: nil, repeats: true)
+      
+        
+    }
+    
+    @objc func countTime() {
+        if timeCount > 0 {
+            timeCount -= 1
+            timeCountTest.text = String(timeCount)
+            if zeroCount <  percnetCount{
+                zeroCount += 1
+                timeBar.progress = Float(zeroCount) / Float(percnetCount)
+                print(timeBar.progress)
+            }
+            
+            
+        }else{
+            timer.invalidate()
+            timeCountLable.text = "Done"
+        }
+        
+
+        
+    }
+    
 
 
 }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+func loveCalculator() {
+    let num = arc4random_uniform(100) + 1
+        
+    switch num {
+    case 100:
+        print("blah")
+    case 99:
+        print("blah blah blah")
+    case 98:
+        print("blah blah blah blah")
+    default:
+        print("huh")
+    }
+}
+
+
+
+loveCalculator()
